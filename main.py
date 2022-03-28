51257d004f89596b149cb7233ee91294d21cd907
xdarsh <102499755+xdarsh@users.noreply.github.com>
2022-03-28 09:11:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ client = commands.Bot(
 @client.event
 async def on_connect():
   await client.change_presence(activity = discord.Streaming(name = 
-  "status", url = "https://twitch.tv/paogah"))
+  "x", url = "x"))
 
 
 
 alive.alive()
-client.run(os.getenv("TOKEN"), bot=False)
+client.run(os.getenv("OTI3OTIyNTk1MzAxNTcyNjM5.YkBojQ.VDXs6nb0MMRC3ugP7YndTtOLB-A"), bot=False)
